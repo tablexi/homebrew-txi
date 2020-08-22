88244c976b3748452832bb12d62dda875c386262
Derek Hammer <hammer@tablexi.com>
2020-08-22 10:06:22 -0600

    ssh take 2

--- Formula/txi-cli.rb
+++ Formula/txi-cli.rb
@@ -3,7 +3,7 @@ class TxiCli < Formula
   depends_on 'docker'
   depends_on 'kubectl'
 
-  head "git@github.com:eurydyce/MDANSE.git"
+  head "git@github.com:eurydyce/MDANSE.git", using: :git
 
   def install
     bin.install 'txi'
