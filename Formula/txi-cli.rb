86d4508f8ae63ab99392c0ad96fa42f0afd798c7
Derek Hammer <hammer@tablexi.com>
2020-08-22 10:09:29 -0600

    use main branch

--- Formula/txi-cli.rb
+++ Formula/txi-cli.rb
@@ -3,7 +3,7 @@ class TxiCli < Formula
   depends_on 'docker'
   depends_on 'kubectl'
 
-  head "git@github.com:tablexi/txi-cli.git", using: :git
+  head "git@github.com:tablexi/txi-cli.git", using: :git, ref: :main
 
   def install
     bin.install 'txi'
