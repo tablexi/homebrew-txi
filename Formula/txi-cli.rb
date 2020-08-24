c3ec17624d1c4561ad0d0bad79c9dd4c97a861e4
Derek Hammer <hammer@tablexi.com>
2020-08-24 16:42:12 -0600

    take 9 git push

--- Formula/txi-cli.rb
+++ Formula/txi-cli.rb
@@ -2,14 +2,13 @@ class TxiCli < Formula
   depends_on 'awscli'
   depends_on 'docker'
   depends_on 'kubectl'
-  depends_on 'rbenv'
 
   head "git@github.com:tablexi/txi-cli.git", using: :git, branch: 'main'
 
   def install
-    ENV['RBENV_ROOT'] = '/Users/derek/.rbenv'
-    system "rbenv local 2.7.1"
-    system "bundle install"
+    # ENV['RBENV_ROOT'] = '/Users/derek/.rbenv'
+    # system "rbenv local 2.7.1"
+    # system "bundle install"
     bin.install "bin/txi"
     prefix.install "lib"
   end
