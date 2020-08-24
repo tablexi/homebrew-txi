1655d51c9b4f9a82cef3c380a6a988b53473e361
Derek Hammer <hammer@tablexi.com>
2020-08-24 16:27:28 -0600

    need xcode

--- Formula/txi-cli.rb
+++ Formula/txi-cli.rb
@@ -2,11 +2,11 @@ class TxiCli < Formula
   depends_on 'awscli'
   depends_on 'docker'
   depends_on 'kubectl'
+  depends_on 'xcode'
 
   head "git@github.com:tablexi/txi-cli.git", using: :git, branch: 'main'
 
   def install
-    system "sudo xcode-select -s /Applications/Xcode.app/Contents/Developer"
     system "bundle install"
     bin.install "bin/txi"
     prefix.install "lib"
