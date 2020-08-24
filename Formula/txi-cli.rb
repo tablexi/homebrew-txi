0ae68bb767275776077bd97dc881e3c81fc089bf
Derek Hammer <hammer@tablexi.com>
2020-08-24 16:40:16 -0600

    take 8 git push

--- Formula/txi-cli.rb
+++ Formula/txi-cli.rb
@@ -7,6 +7,7 @@ class TxiCli < Formula
   head "git@github.com:tablexi/txi-cli.git", using: :git, branch: 'main'
 
   def install
+    ENV['RBENV_ROOT'] = '/Users/derek/.rbenv'
     system "rbenv local 2.7.1"
     system "bundle install"
     bin.install "bin/txi"
