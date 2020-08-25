fa5bb135e595096e8192a65e46927dcc61d6024a
Derek Hammer <hammer@tablexi.com>
2020-08-25 08:09:45 -0600

    Install gem with dependencies

--- Formula/txi-cli.rb
+++ Formula/txi-cli.rb
@@ -2,11 +2,39 @@ class TxiCli < Formula
   depends_on 'awscli'
   depends_on 'docker'
   depends_on 'kubectl'
+  depends_on 'ruby'
 
   head "git@github.com:tablexi/txi-cli.git", using: :git, branch: 'main'
 
   def install
+    ENV['GEM_HOME']="#{prefix}"
+    ENV['GEM_PATH']="#{prefix}"
+    if defined?(HOMEBREW_SHIMS_PATH)
+      ENV['PATH'] = ENV['PATH'].sub(HOMEBREW_SHIMS_PATH.to_s, '/usr/local/bin')
+    end
+
     system "gem build txi-cli.gemspec"
-    system "gem install txi-cli*.gem --install-dir #{prefix}"
+    system "gem", "install", "txi-cli*.gem",
+             "--no-document",
+             "--no-wrapper",
+             "--no-user-install",
+             "--install-dir", prefix,
+             "--bindir", bin
+
+    gemspec = Gem::Specification::load("txi-cli.gemspec")
+    ruby_libs = Dir.glob("#{prefix}/gems/*/lib")
+    gemspec.executables.each do |exe|
+      file = Pathname.new("#{brew_gem_prefix}/#{gemspec.bindir}/#{exe}")
+      (bin+file.basename).open('w') do |f|
+        f << <<-RUBY
+#!/usr/bin/ruby --disable-gems
+ENV['GEM_HOME']="#{prefix}"
+ENV['GEM_PATH']="#{prefix}"
+require 'rubygems'
+$:.unshift(#{ruby_libs.map(&:inspect).join(",")})
+load "#{file}"
+        RUBY
+      end
+    end
   end
 end
